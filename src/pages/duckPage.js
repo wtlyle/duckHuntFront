c54862365ffdfefde575c6752abe05afc5d70d6b
dlim <dlim87@gmail.com>
2018-11-19 17:08:01 -0800

    add comments, but broken w/o

--- src/pages/duckPage.js
+++ src/pages/duckPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getDuck } from "../api"
+import { getDuck, getComment } from "../api"
 
 class DuckPage extends Component {
   constructor(props){
@@ -7,18 +7,26 @@ class DuckPage extends Component {
 
     this.state={
         duck: {
-        name: '',
-        age: '',
-        enjoys: '',
-        fullImage: ''
-      }
+          name: '',
+          age: '',
+          enjoys: '',
+          fullImage: '',
+          quacks: ''
+        },
+        comments: [],
+        allSet: false
     }
   }
   render() {
-    let { allDuckies } = this.props
     let id = this.props.match.params.id
     let { name, age, enjoys, fullImage } = this.state.duck
-    console.log(allDuckies);
+    console.log(this.state.comments);
+    // if(this.state.comments.length > 0){
+    //   let { body, author } = this.state.comments[Math.floor(Math.random()*this.state.comments.length-1)]
+    // }else{
+    //   let body = 'No one has commented on this duck yet.'
+    //   let author = 'You should!'
+    // }
     return (
       <div>
         <div id="profile">
@@ -30,6 +38,11 @@ class DuckPage extends Component {
                 <h3>Age: {age}</h3>
                 <p>Enjoys: {enjoys}</p>
               </div>
+              <div id="comment">
+                <h4>What other ducks are saying about {this.state.duck.name}:</h4>
+                {this.state.allSet && <p>"{this.state.comments[0].body}"</p>}
+                {this.state.allSet && <p>from {this.state.comments[0].author}</p>}
+              </div>
             </div>
           </div>
         </div>
@@ -40,7 +53,11 @@ class DuckPage extends Component {
   componentDidMount(){
     getDuck(this.props.match.params.id)
     .then(resp => {
-      this.setState({duck:resp})
+      if (resp.comments.length >= 0){
+        this.setState({duck:resp.duck, comments: resp.comments, allSet: true})
+      }else {
+        this.setState({duck:resp.duck, comments: {body: "No one has commented on this duck yet.", author: "You should!"}, allSet: true})
+      }
     })
   }
 
--- src/pages/ducks.js
+++ src/pages/ducks.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import DuckTale from '../components/ducktale.js'
-import { getDuck } from '../api'
+import { getDucks } from '../api'
 
 
 class Ducks extends Component {
   constructor(props){
     super(props)
+
+    this.state = {
+      allDuckies: []
+    }
   }
 
   render() {
-    console.log(this.props.allDuckies);
-    let duckies = this.props.allDuckies.map((duck,i) => {
+    console.log(this.state.allDuckies);
+    let duckies = this.state.allDuckies.map((duck,i) => {
       console.log("i'm doing stuff");
        return <DuckTale id={i + 1} info={duck} />
     })
@@ -23,6 +27,13 @@ class Ducks extends Component {
       </main>
     );
   }
+  componentDidMount() {
+    console.log("firing getDucks");
+       getDucks()
+       .then(APIducks => {
+           this.setState({allDuckies: APIducks})
+       })
+   }
 }
 
 export default Ducks;
